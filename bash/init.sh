89b6092d5053732a96673ed2248b4118825189a9
yipingx <summer_xyp@qq.com>
2019-10-13 16:24:24 +0800

    add ll

--- bash/init.sh
+++ bash/init.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-export PATH="/home/yipingx/software/Sencha/cmd/6.6.0.13:$PATH"
+#export PATH="/home/yipingx/software/Sencha/cmd/6.6.0.13:$PATH"
 alias gitlog='git log --pretty=one --graph --date-order'
+
+alias ll='ls -la'
+
+
 
